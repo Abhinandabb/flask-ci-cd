4e1b85063a8345c6eb6e3a66984440fc44dd3592
Abhinand <abhinand2018abhi@gmail.com>
2026-01-30 12:45:28 +0530

    changes made

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify({"message": "Docker CI/CD is working 🚀"})
+    return jsonify({"message": "Docker CI/CD is working 🚀 not hahaaaaaa"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
